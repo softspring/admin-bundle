4de009fc94d7f25be235d2d4087ffe435c6fbf79
Javi H. Gil <javihgil@gmail.com>
2019-10-18 10:19:33 +0200

    Update event classes

--- Event/GetResponseEntityEvent.php
+++ Event/GetResponseEntityEvent.php
@@ -2,8 +2,8 @@
 
 namespace Softspring\AdminBundle\Event;
 
-use Softspring\ExtraBundle\Event\GetResponseEventInterface;
-use Softspring\ExtraBundle\Event\GetResponseTrait;
+use Softspring\CoreBundle\Event\GetResponseEventInterface;
+use Softspring\CoreBundle\Event\GetResponseTrait;
 
 class GetResponseEntityEvent extends EntityEvent implements GetResponseEventInterface
 {
--- Event/GetResponseFormEvent.php
+++ Event/GetResponseFormEvent.php
@@ -2,8 +2,8 @@
 
 namespace Softspring\AdminBundle\Event;
 
-use Softspring\ExtraBundle\Event\GetResponseEventInterface;
-use Softspring\ExtraBundle\Event\GetResponseTrait;
+use Softspring\CoreBundle\Event\GetResponseEventInterface;
+use Softspring\CoreBundle\Event\GetResponseTrait;
 
 class GetResponseFormEvent extends FormEvent implements GetResponseEventInterface
 {
